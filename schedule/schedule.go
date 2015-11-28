7608845a10609364872594c1b211a571a90b5d13
Mathias Fredriksson <mafredri@gmail.com>
2015-11-28 11:46:21 +0200

    Unexport structs in schedule

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -13,35 +13,33 @@ func Run(schedule RunnableSchedule, maxDepth int) float64 {
 	return schedule.Run(0, maxDepth)
 }
 
-// Step is a step function in the schedule.
-type Step struct {
+type step struct {
 	input    int // Input for function
 	function func(i int) float64
 }
 
-// NewStep returns a step with provided function and input.
-func NewStep(function func(i int) float64, input int) Step {
-	return Step{input, function}
+// NewStep returns a step in the schedule with provided function and input.
+func NewStep(function func(i int) float64, input int) RunnableSchedule {
+	return step{input, function}
 }
 
 // Run runs the step function with the input and returns the delta.
-func (s Step) Run(depth, maxDepth int) float64 {
+func (s step) Run(depth, maxDepth int) float64 {
 	delta := s.function(s.input)
 	return delta
 }
 
-// Sequence is a sequence of runnable schedules.
-type Sequence struct {
+type sequence struct {
 	sequences []RunnableSchedule
 }
 
 // NewSequence returns a new sequence of runnable schedules.
-func NewSequence(sequences []RunnableSchedule) Sequence {
-	return Sequence{sequences}
+func NewSequence(sequences []RunnableSchedule) RunnableSchedule {
+	return sequence{sequences}
 }
 
 // Run runs all runnable schedules in a sequence. The largest delta from any of the runnable schedules is returned.
-func (s Sequence) Run(depth, maxDepth int) float64 {
+func (s sequence) Run(depth, maxDepth int) float64 {
 	var delta float64
 	for _, s := range s.sequences {
 		delta = math.Max(delta, s.Run(depth+1, maxDepth))
@@ -50,22 +48,21 @@ func (s Sequence) Run(depth, maxDepth int) float64 {
 	return delta
 }
 
-// Loop is a runnable schedule that runs itself.
-type Loop struct {
+type loop struct {
 	schedule RunnableSchedule
 	maxDelta float64
 }
 
-// NewLoop returns a new loop.
-func NewLoop(schedule RunnableSchedule, maxDelta float64) Loop {
-	return Loop{schedule, maxDelta}
+// NewLoop returns a new runnable loop schedule.
+func NewLoop(schedule RunnableSchedule, maxDelta float64) RunnableSchedule {
+	return loop{schedule, maxDelta}
 }
 
 // Run reruns the loop until a desired delta (maxDelta) is reached.
-func (sl Loop) Run(depth, maxDepth int) float64 {
+func (l loop) Run(depth, maxDepth int) float64 {
 	delta := math.MaxFloat64
-	for delta > sl.maxDelta {
-		delta = sl.schedule.Run(depth+1, maxDepth)
+	for delta > l.maxDelta {
+		delta = l.schedule.Run(depth+1, maxDepth)
 	}
 
 	return delta
